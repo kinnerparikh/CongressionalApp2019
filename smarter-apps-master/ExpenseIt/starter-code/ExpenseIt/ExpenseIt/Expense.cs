97c6e8036a784ee536de729ceb34da74b15fda9f
Kinner Parikh <kinnerpa@gmail.com>
2019-10-26 16:36:13 -0700

    Initial Commit

---
+++ smarter-apps-master/ExpenseIt/starter-code/ExpenseIt/ExpenseIt/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media.Abstractions;
+
+namespace InvoiceIt
+{
+    public class Invoice
+    {
+        public double Total { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Photo { get; set; }
+    }
+}
